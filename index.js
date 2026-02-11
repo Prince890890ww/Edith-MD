a8d187b19b2406f1a98563ac852196ae6935d7a4
ArslanMDOfficialByTesting <muhammadarslanmdofficial@gmail.com>
2026-02-11 20:39:14 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-/*
-   ✦ ✦ ✦   E D I T H - M D   ✦ ✦ ✦
-
-─────────────────────────────────────────────────────────────
-📛 Project   : Edith-MD
-👤 Creator   : Bandaheali
-🌐 GitHub    : https://github.com/Bandah-E-Ali
-📱 Contact   : https://t.me/@bandaheali
-📢 Channel   : https://whatsapp.com/channel/0029VajGHyh2phHOH5zJl73P
-🗓 Release   : 24 • November • 2025 | 02:00 PM
-─────────────────────────────────────────────────────────────
-
-//   ⭐ PROUDLY MADE IN PAKISTAN ⭐
-*/
 
 
 
